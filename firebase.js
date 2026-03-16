db399956799836e53a09ab73c0d6e49a2a67430b
Mona9324 <qoekfthsu9@gmail.com>
2026-03-16 15:27:54 +0900

    firebase.js

--- firebase.js
+++ firebase.js
@@ -1,5 +1,5 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyA90OVkTb5svXjHrF37wI_0dUWv-4Pr7Xc",
+  apiKey: "YOUR_API_KEY",
   authDomain: "svs-booking-4a785.firebaseapp.com",
   projectId: "svs-booking-4a785",
   storageBucket: "svs-booking-4a785.firebasestorage.app",
@@ -7,6 +7,8 @@ const firebaseConfig = {
   appId: "1:815196694859:web:eb39b55526841c248860ce"
 };
 
-firebase.initializeApp(firebaseConfig);
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
 
 window.db = firebase.firestore();
